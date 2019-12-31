a2dad7c13e25166f6ee26c1de9dc5d95e305f89c
Kamil <vaskkr@gmail.com>
2019-12-31 16:30:53 +0100

    refactor, creating ratings moved to class as static method

--- scrapper.py
+++ scrapper.py
@@ -19,6 +19,18 @@ class Rating:
 
         return "\n".join((line_1, line_2, line_3, line_4, ""))
 
+    @staticmethod
+    def create_rating_from_match(match):
+        title = html.unescape(match.group(1))
+        release_year = match.group(2)
+        avg_rating = match.group(3)
+        cnt_rating = match.group(4)
+        date_rated = match.group(5)
+        rating = match.group(6)
+
+        r = Rating(title, release_year, avg_rating, cnt_rating, date_rated, rating)
+        return r
+
 
 rating = re.compile(r'(.*) (\d{4}).*\s'                                                         # tytuł i rok produkcji
                     r'(?:Oglądaj .*\s)?'
@@ -38,17 +50,10 @@ page_count = len(count_pages.findall(text))
 
 counter = 0
 
+
 for match in matches:
     counter += 1
-
-    title = html.unescape(match.group(1))
-    release_year = match.group(2)
-    avg_rating = match.group(3)
-    cnt_rating = match.group(4)
-    date_rated = match.group(5)
-    rating = match.group(6)
-
-    r = Rating(title, release_year, avg_rating, cnt_rating, date_rated, rating)
+    r = Rating.create_rating_from_match(match)
     print(str(r))
 
 print("Znaleziono " + str(counter) + " filmów na " + str(page_count) + " stronach")
