8a62520fcd850413984cdfa6ee29eadb1ea60013
Kamil <vaskkr@gmail.com>
2020-01-04 08:29:10 +0100

    default out is csv now, error handling

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import re
 import html
+import datetime
 
 
 class Rating:
@@ -16,15 +17,23 @@ class Rating:
         self.ratings_average = ratings_average
         self.ratings_count = ratings_count
         self.date_rated = date_rated
+        if re.search(r'\d{4}', self.date_rated) is None:
+            self.date_rated += ' ' + str(datetime.datetime.now().year)
         self.personal_rating = rating
 
     def __str__(self):
-        line_1 = " ".join((self.title, self.release_year))
-        line_2 = " ".join(("oceniono", self.ratings_count, "razy,", "średnia", self.ratings_average))
-        line_3 = " ".join(("ocena:", self.personal_rating))
-        line_4 = " ".join(("data oceny:", self.date_rated))
+        line_1 = ' '.join((self.title, self.release_year))
+        line_2 = ' '.join(("oceniono", self.ratings_count, "razy,", "średnia", self.ratings_average))
+        line_3 = ' '.join(("ocena:", self.personal_rating))
+        line_4 = ' '.join(("data oceny:", self.date_rated))
 
-        return "\n".join((line_1, line_2, line_3, line_4, ""))
+        return '\n'.join((line_1, line_2, line_3, line_4, ''))
+
+    def to_csv(self):
+        return ', '.join(('\"' + self.title + '\"',
+                          self.release_year,
+                          self.ratings_average.replace(',', '.'), self.ratings_count,
+                          self.date_rated, self.personal_rating))
 
     @staticmethod
     def create_rating_from_match(match):
@@ -46,7 +55,7 @@ class WantToSee:
         self.year = year
 
     def __str__(self):
-        return str(self.title) + " " + str(self.year)
+        return str(self.title) + ' ' + str(self.year)
 
     @staticmethod
     def create_from_match(match):
@@ -55,24 +64,29 @@ class WantToSee:
 
 def main():
     count_pages = re.compile("filmweb.pl")
+    text = ""
 
-    file = open("in", 'r', encoding='utf-8')
-    text = file.read()
-    file.close()
+    try:
+        with open("in.txt", 'r', encoding='utf-8') as file:
+            text = file.read()
+    except FileNotFoundError:
+        print("Plik \"in.txt\" nie został znaleziony")
+        exit(1)
 
     matches = Rating.regex.finditer(text)
     page_count = len(count_pages.findall(text))
 
     counter = 0
 
-    for match in matches:
-        counter += 1
-        r = Rating.create_rating_from_match(match)
-        print(str(r))
+    with open("out.txt", 'w+', encoding='utf-8') as out:
+        for match in matches:
+            counter += 1
+            r = Rating.create_rating_from_match(match)
+            out.write(r.to_csv() + '\n')
 
     print("Znaleziono " + str(counter) + " filmów na " + str(page_count) + " stronach")
     wait = input("Wciśnij ENTER aby zakończyć")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
