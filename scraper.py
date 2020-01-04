1db81464075bbd4bff240763acd311027ad90a53
Kamil <vaskkr@gmail.com>
2020-01-04 08:58:24 +0100

    handling of relative dates shown on the site

--- scraper.py
+++ scraper.py
@@ -3,6 +3,24 @@ import html
 import datetime
 
 
+def int_into_polish_month(month_int):
+    months = {
+        1: "stycznia",
+        2: "lutego",
+        3: "marca",
+        4: "kwietnia",
+        5: "maja",
+        6: "czerwca",
+        7: "lipca",
+        8: "sierpnia",
+        9: "września",
+        10: "października",
+        11: "listopada",
+        12: "grudnia"
+    }
+    return months[month_int]
+
+
 class Rating:
     regex = re.compile(r'(.*) (\d{4}).*\s'                                                     # tytuł i rok produkcji
                        r'(?:Oglądaj .*\s)?'
@@ -11,14 +29,26 @@ class Rating:
                        r'(dzisiaj|wczoraj|\d{1,2} \w*(?: \d{4})?)\s'                           # dzień i miesiąc oceny
                        r'(\d{1,2})', flags=re.U)                                               # ocena
 
+    @staticmethod
+    def datetime_into_date(date):
+        return str(date.day) + ' ' + int_into_polish_month(date.month) + ' ' + str(date.year)
+
     def __init__(self, title, release_year, ratings_average, ratings_count, date_rated, rating):
         self.title = title
         self.release_year = release_year
         self.ratings_average = ratings_average
         self.ratings_count = ratings_count
-        self.date_rated = date_rated
-        if re.search(r'\d{4}', self.date_rated) is None:
-            self.date_rated += ' ' + str(datetime.datetime.now().year)
+        if date_rated == "dzisiaj":
+            today = datetime.datetime.now()
+            self.date_rated = Rating.datetime_into_date(today)
+        else:
+            if date_rated == "wczoraj":
+                yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+                self.date_rated = Rating.datetime_into_date(yesterday)
+            else:
+                self.date_rated = date_rated
+                if re.search(r'\d{4}', self.date_rated) is None:
+                    self.date_rated += ' ' + str(datetime.datetime.now().year)
         self.personal_rating = rating
 
     def __str__(self):
