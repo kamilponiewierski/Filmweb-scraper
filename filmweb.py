d8d148edbb0dc4e0b8d7ef79fdada3d51dfca7ac
Kamil <vaskkr@gmail.com>
2020-01-09 17:42:57 +0100

    moved Movie up

--- filmweb.py
+++ filmweb.py
@@ -4,25 +4,6 @@ import re
 import typing
 
 
-class WantToSee(object):
-    regex = re.compile(r'(.*)(.* \d{4})\s'  # tytuł i rok
-                       r'(?:.*\s)?'  # tytuł oryginalny
-                       r'((\d{1,2} godz.)* ?(\d{1,2} min.)*)\s'  # czas trwania
-                       r'(?:Oglądaj.*\s)?'  # reklama
-                       r'(\d,\d) ([\d ]*)oceny?')  # średnia, liczba ocen
-
-    def __init__(self, title, year):
-        self.title = title
-        self.year = year
-
-    def __str__(self):
-        return str(self.title) + ' ' + str(self.year)
-
-    @staticmethod
-    def create_from_match(match):
-        return WantToSee(match.group(1), match.group(2))
-
-
 class Movie:
     def __init__(self, title: str, release_year: str):
         self.title = title
@@ -60,6 +41,25 @@ class Movie:
                 list_of_lists.pop(0)
 
             return acc
+
+
+class WantToSee(object):
+    regex = re.compile(r'(.*)(.* \d{4})\s'  # tytuł i rok
+                       r'(?:.*\s)?'  # tytuł oryginalny
+                       r'((\d{1,2} godz.)* ?(\d{1,2} min.)*)\s'  # czas trwania
+                       r'(?:Oglądaj.*\s)?'  # reklama
+                       r'(\d,\d) ([\d ]*)oceny?')  # średnia, liczba ocen
+
+    def __init__(self, title, year):
+        self.title = title
+        self.year = year
+
+    def __str__(self):
+        return str(self.title) + ' ' + str(self.year)
+
+    @staticmethod
+    def create_from_match(match):
+        return WantToSee(match.group(1), match.group(2))
 
 
 class Rating:
