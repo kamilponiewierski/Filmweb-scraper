c2863373cf3893a6b0f7f16b91b27111230a1967
Kamil <vaskkr@gmail.com>
2020-01-09 14:17:57 +0100

    regex improvement

--- WantToSee.py
+++ WantToSee.py
@@ -2,7 +2,11 @@ import re
 
 
 class WantToSee(object):
-    regex = re.compile(r'(?:.*\n)(.* \d{4})\s')
+    regex = re.compile(r'(?:.*)(.* \d{4})\s'  # tytuł i rok
+                       r'(?:.*)?\s?'  # tytuł oryginalny
+                       r'((\d{1,2} godz.)* ?(\d{1,2} min.)*)\s'  # czas trwania
+                       r'(?:Oglądaj.*\s)?'  # reklama
+                       r'(\d,\d) ([\d ]*)oceny?')  # średnia, liczba ocen
 
     def __init__(self, title, year):
         self.title = title
