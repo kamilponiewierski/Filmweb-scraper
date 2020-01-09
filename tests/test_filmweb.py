240bc7ef4c10fa3eaa99abedd07b72879852aa70
Kamil <vaskkr@gmail.com>
2020-01-09 17:08:24 +0100

    tests moved to one file

--- tests/test_filmweb.py
+++ tests/test_filmweb.py
@@ -1,8 +1,8 @@
-from unittest import TestCase
-from filmweb import Movie
+import unittest
+from filmweb import Movie, Rating
 
 
-class TestMovie(TestCase):
+class TestMovie(unittest.TestCase):
     def test_Movie_eq(self):
         movie_1 = Movie("Rudobrody", "1965")
         movie_2 = Movie("Rudobrody", "1965")
@@ -73,3 +73,24 @@ class TestMovie(TestCase):
 
     def test_intersection_of_many_empty(self):
         self.assertEqual(Movie.intersection_of_many([]), set())
+
+
+class TestRating(unittest.TestCase):
+    def test_create_rating_from_match(self):
+        with open("sample_page", encoding='utf-8') as test_page:
+            text = test_page.read()
+            matches = Rating.regex.finditer(text)
+            for match in matches:
+                movie = Rating.create_rating_from_match(match)
+                if movie.movie.title == "Han Solo: Gwiezdne wojny - historie":
+                    self.assertEqual(movie.personal_rating, 7, "Rating is int")
+                    self.assertEqual(movie.date_rated, "22 września 2018")
+                    self.assertEqual(movie.ratings_average, "6,8", "Average is str")
+
+    def test_to_csv(self):
+        test = Rating(Movie("Niebo i piekło", "1963"), "8,0", "2 241", "17 września 2018", 9)
+        self.assertEqual(test.to_csv(), "\"Niebo i piekło\", 1963, 8.0, 2 241, 17 września 2018, 9")
+
+
+if __name__ == '__main__':
+    unittest.main()
