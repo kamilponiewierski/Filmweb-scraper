0710dc75e678eb5527b8ccc86f73577637aa3f17
Kamil <vaskkr@gmail.com>
2020-01-25 15:12:42 +0100

    scraping from every file starting with "in" and ending with ".txt"

--- rating_scraper.py
+++ rating_scraper.py
@@ -1,28 +1,29 @@
 import re
+import os
 
 from filmweb import Rating
 
 
 def main():
     count_pages_regex = re.compile("filmweb.pl")
-
-    try:
-        with open("in.txt", 'r', encoding='utf-8') as file:
-            text = file.read()
-    except FileNotFoundError:
-        print("Plik \"in.txt\" nie został znaleziony")
-        exit(1)
-
-    matches = Rating.regex.finditer(text)
-    page_count = len(count_pages_regex.findall(text))
+    path = "./"
 
     counter = 0
+    page_count = 0
 
-    with open("out.txt", 'w+', encoding='utf-8') as out:
-        for match in matches:
-            counter += 1
-            r = Rating.create_rating_from_match(match)
-            out.write(r.to_csv() + '\n')
+    for f in os.listdir(path):
+        if re.match(r'in.*\.txt', f):
+            with open(f, 'r', encoding='utf-8') as file:
+                text = file.read()
+
+            matches = Rating.regex.finditer(text)
+            page_count += len(count_pages_regex.findall(text))
+
+            with open("out" + f[2:], 'w+', encoding='utf-8') as out:
+                for match in matches:
+                    counter += 1
+                    r = Rating.create_rating_from_match(match)
+                    out.write(r.to_csv() + '\n')
 
     print("Znaleziono " + str(counter) + " filmów na " + str(page_count) + " stronach")
     input("Wciśnij ENTER aby zakończyć")
